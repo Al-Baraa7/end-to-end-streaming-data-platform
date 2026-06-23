184ed3d1bd0b197a577fe2e134590e8ff488cf75
al-baraa-al-ahdal <5albaraaa515@gmaile.com>
2026-06-23 14:30:05 +0300

    Add EDA methodology and explore Postgres tables (users, subscriptions, payments)

--- scripts/gcs/setup_storage.py
+++ scripts/gcs/setup_storage.py
@@ -9,12 +9,14 @@ client = storage.Client(
     }
 )
 
-bucket_name = "end-to-end-streaming-data-platform"
+# bucket_name = "end-to-end-streaming-data-platform"
+bucket_name_silver = "end-to-end-streaming-data-platform-silver"
+bucket_name_gold = "end-to-end-streaming-data-platform-gold"
+for name in [bucket_name_silver,bucket_name_gold]:   
+    try:
+        bucket=client.create_bucket(name)
+        print(f"Bucket created: {name}")
 
-try:
-    bucket=client.create_bucket(bucket_name)
-    print(f"Bucket created: {bucket_name}")
-
-except Exception as e:
-    bucket= client.bucket(bucket_name)
-    print(f"ERROR: {e}") 
+    except Exception as e:
+        bucket= client.bucket(name)
+        print(f"ERROR: {e}") 
